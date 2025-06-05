b71c0bd95364e7f478e7f2620a254767d32e624d
AshleeHODOR <hodor1987@yahoo.com>
2025-06-05 12:30:43 -0700

    109 - Flask and for loops

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 server = Flask('todolist')
+
+todo_list = ["Clean cat litter", "Mail package", "Wash Bob"]
 
 
 @server.get("/") #/ root page (home)
 def home():
-    return render_template('home.html')
+    return render_template("home.html", todo_list=todo_list)
+
+@server.post("/add")
+def add_todo():
+    print("hello from /add")
+    todo_item = request.form.get("todo")
+
+    if todo_item:
+        todo_list.append(todo_item)
+
+    return redirect(url_for("home")) 
+
 
 @server.get("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
 @server.get("/contact")
 def contact():

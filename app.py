955a65c1472d820e643bf44a0509f5fb7fbf2f79
AshleeHODOR <hodor1987@yahoo.com>
2025-06-07 15:30:12 -0700

    109 - FINAL project

--- app.py
+++ app.py
@@ -28,6 +28,20 @@ def about():
 def contact():
     return render_template("contact.html")
 
+all_images = []
+
+@server.get("/gallery")
+def gallery():
+    return render_template("gallery.html", all_images=all_images)
+
+
+@server.post("/addImage")
+def add_image():
+    image_url = request.form.get("image")
+    all_images.append(image_url)
+    return redirect(url_for("gallery"))
+
+
 
 
 
